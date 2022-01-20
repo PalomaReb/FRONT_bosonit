488cb713f3eeb8226ceb839599a5df13591baeb2
Paloma Rebolleda <paloma.rebolleda@gmail.com>
2022-01-20 17:15:42 +0100

    person component

---
+++ ex6-person-crud/person-crud/src/app/components/person/person.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Person } from 'src/app/persons';
+
+@Component({
+  selector: 'app-person',
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.css']
+})
+export class PersonComponent implements OnInit {
+person:Person[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getPerson():void{
+    
+  }
+}
