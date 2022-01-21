01b4405344a576877fdf98cfcc2f8f2bd5d2f2fd
Paloma Rebolleda <paloma.rebolleda@gmail.com>
2022-01-21 12:07:40 +0100

    person detail component created

--- ex6-person-crud/person-crud/src/app/app.module.ts
+++ ex6-person-crud/person-crud/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonComponent } from './components/person/person.component';
 import { PersonSearchComponent } from './components/person-search/person-search.component';
+import { PersonDetailComponent } from './person-detail/person-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonComponent,
     PersonSearchComponent,
+    PersonDetailComponent,
   ],
   imports: [
     BrowserModule,
