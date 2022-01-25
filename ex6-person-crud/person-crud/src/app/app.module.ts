9bb6fcf16f2f5f07b889d8cac0288c3b18a2d4a5
Paloma Rebolleda <paloma.rebolleda@gmail.com>
2022-01-25 17:24:37 +0100

    modulos updated

--- ex6-person-crud/person-crud/src/app/app.module.ts
+++ ex6-person-crud/person-crud/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PersonComponent } from './components/person/person.component';
 import { PersonSearchComponent } from './components/person-search/person-search.component';
-import { PersonDetailComponent } from './person-detail/person-detail.component';
+import { PersonDetailComponent } from './components/person-detail/person-detail.component';
 
 @NgModule({
   declarations: [
--- ex6-person-crud/person-crud/src/app/components/person-detail/person-detail.component.ts
+++ ex6-person-crud/person-crud/src/app/components/person-detail/person-detail.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Person } from 'src/app/persons';
+import { PersonService } from 'src/app/person.service';
+
 
 @Component({
   selector: 'app-person-detail',
@@ -6,10 +12,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./person-detail.component.css']
 })
 export class PersonDetailComponent implements OnInit {
+@Input() person? : Person;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private personService: PersonService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
+   // this.getPerson();
   }
 
+  // getPerson(): void {
+  //   const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
+  //   this.personService.getPerson()
+  //     .subscribe(person => this.person = person);
+  // }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    if (this.person) {
+      this.personService.updatePerson(this.person)
+        .subscribe(() => this.goBack());
+    }
+  }
 }
